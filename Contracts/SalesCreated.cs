a86c537467d7e19c212d83dae7405ddfc04596ca
Omar Ledesma <joseomar.ledagui@gmail.com>
2024-08-25 21:32:42 +0100

    create the events to the service bus

---
+++ Contracts/SalesCreated.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public class SalesCreated
+    {
+        public Guid Id { get; set; }
+        public int Price { get; set; }
+        public int ReservePrice { get; set; }
+        public int SoldPrice { get; set; }
+        public string Seller { get; set; }
+        public string Buyer { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public string Status { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        public string Quality { get; set; }
+        public string Image { get; set; }
+    }
+}
