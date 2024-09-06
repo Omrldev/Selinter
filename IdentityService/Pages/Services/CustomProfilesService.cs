535f2a799795e4ee4517b11642156aa9a8c4d653
Omar Ledesma <joseomar.ledagui@gmail.com>
2024-09-06 16:51:36 +0100

    add claims to the token

---
+++ IdentityService/Pages/Services/CustomProfilesService.cs
@@ -0,0 +1,38 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using IdentityModel;
+using IdentityService.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace IdentityService.Pages.Services
+{
+    public class CustomProfilesService : IProfileService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public CustomProfilesService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            var user = await _userManager.GetUserAsync(context.Subject);
+            var existingClaim = await _userManager.GetClaimsAsync(user);
+
+            var claims = new List<Claim>
+            {
+                new("username", user.UserName)
+                //new Claim("username", user.UserName)
+            };
+
+            context.IssuedClaims.AddRange(claims);
+            context.IssuedClaims.Add(existingClaim
+                .FirstOrDefault(x => x.Type == JwtClaimTypes.Name));
+        }
+
+        public Task IsActiveAsync(IsActiveContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
